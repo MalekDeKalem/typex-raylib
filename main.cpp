8eccc85fc6bc1679cc37e8987bd8d2da68d66fe3
dorian <dimitri200169@gmail.com>
2025-03-02 20:39:16 +0100

    gamescreen enum

--- main.cpp
+++ main.cpp
@@ -32,6 +32,11 @@ typedef enum StateChar {
 	FALSE,
 } StateChar;
 
+typedef enum GameScreen {
+  TITLE,
+  GAMEPLAY,
+} GameScreen;
+
 
 const std::vector<std::string> Dict = {
 	"interesting", "mother", "father", "gallant", "robber", "paleontologist", 
@@ -79,6 +84,7 @@ int main(void)
 	int start_y = SCREEN_HEIGHT / 2 - text_size.y / 2;
 
 	std::vector<StateChar> state_of_char(selected_word.size(), StateChar::DEFAULT);
+  GameScreen currentScreen = GameScreen::TITLE;
 
 
 	bool is_correct = false;
@@ -90,71 +96,92 @@ int main(void)
 
 		// if the correct char is pressed increment the highlight_index and set is_correct to true
 		// if the false char is pressed increment the highlight_index and set is_correct to false;
+
+    switch(currentScreen) {
+      case TITLE: 
+      {
+        if (IsKeyPressed(KEY_ENTER)) {
+          currentScreen = GameScreen::GAMEPLAY;
+        }
+           BeginDrawing();
+            ClearBackground(SKYBLUE);
+            DrawText("Typex", 20, 20, 20, RAYWHITE);
+            DrawText("Press ENTER to play", 20, 50, 20, RAYWHITE);
+          EndDrawing();
+      } break;
+      case GAMEPLAY:
+      {
+        int key_pressed = GetCharPressed();	
+
+          if (key_pressed != 0) {
+
+            if (key_pressed == selected_word[highlight_index]) {
+              std::cout << "Pressed correct key" << std::endl;
+              state_of_char[highlight_index] = StateChar::CORRECT;
+            } else {
+              std::cout << "Pressed incorrect key" << std::endl;
+              state_of_char[highlight_index] = StateChar::FALSE;
+              lives--;
+            }
+
+            highlight_index++;
+          }
+
+          BeginDrawing();
+            ClearBackground(SKYBLUE);
+            int x_offset = 0;
+            for (size_t i = 0; i < selected_word.length(); i++) {
+              std::string charStr(1, selected_word[i]);
+              int char_width = MeasureText(charStr.c_str(), font_size);
+
+              switch (state_of_char[i]) {
+                case StateChar::DEFAULT:
+                  DrawText(charStr.c_str(), start_x + x_offset, start_y, font_size, RAYWHITE);
+                  break;
+                case StateChar::CORRECT:
+                  DrawText(charStr.c_str(), start_x + x_offset, start_y, font_size, GREEN);
+                  break;
+                case StateChar::FALSE:
+                  DrawText(charStr.c_str(), start_x + x_offset, start_y, font_size, RED);
+                  break;
+                default:
+                  std::cout << "Unknown state" << std::endl;
+                  break;
+              }
+
+              x_offset += char_width + spacing;
+            }
+            std::string score_str = std::to_string(score);
+            const char* score_text = score_str.c_str();
+            std::string lives_str = std::to_string(lives);
+            const char* lives_text = lives_str.c_str();
+            DrawText(score_text, 20, SCREEN_HEIGHT - 20, 20, RAYWHITE);
+            DrawText(lives_text, SCREEN_WIDTH-20, SCREEN_HEIGHT - 20, 20, RAYWHITE);
+          EndDrawing();
+
+
+
+          if (highlight_index >= selected_word.size()) {
+            highlight_index = 0;
+            int rand_index = distr(gen);
+            selected_word = Dict[rand_index];
+            state_of_char.assign(selected_word.size(), StateChar::DEFAULT);
+            text_size = MeasureTextEx(GetFontDefault(), selected_word.c_str(), font_size, spacing);
+            start_x = SCREEN_WIDTH / 2 - text_size.x / 2;
+            start_y = SCREEN_HEIGHT / 2 - text_size.y / 2;
+            score++;
+          }
+
+          if (lives <= 0) currentScreen = GameScreen::TITLE;
+          
+
+      } break;
+
+      default: break;
+
+    }
 		
-		int key_pressed = GetCharPressed();	
-
-		if (key_pressed != 0) {
-
-			if (key_pressed == selected_word[highlight_index]) {
-				std::cout << "Pressed correct key" << std::endl;
-				state_of_char[highlight_index] = StateChar::CORRECT;
-			} else {
-				std::cout << "Pressed incorrect key" << std::endl;
-				state_of_char[highlight_index] = StateChar::FALSE;
-				lives--;
-			}
-
-			highlight_index++;
-		}
-
-		BeginDrawing();
-			ClearBackground(SKYBLUE);
-			int x_offset = 0;
-			for (size_t i = 0; i < selected_word.length(); i++) {
-				std::string charStr(1, selected_word[i]);
-				int char_width = MeasureText(charStr.c_str(), font_size);
-
-				switch (state_of_char[i]) {
-					case StateChar::DEFAULT:
-						DrawText(charStr.c_str(), start_x + x_offset, start_y, font_size, RAYWHITE);
-						break;
-					case StateChar::CORRECT:
-						DrawText(charStr.c_str(), start_x + x_offset, start_y, font_size, GREEN);
-						break;
-					case StateChar::FALSE:
-						DrawText(charStr.c_str(), start_x + x_offset, start_y, font_size, RED);
-						break;
-					default:
-						std::cout << "Unknown state" << std::endl;
-						break;
-				}
-
-				x_offset += char_width + spacing;
-			}
-			std::string score_str = std::to_string(score);
-			const char* score_text = score_str.c_str();
-			std::string lives_str = std::to_string(lives);
-			const char* lives_text = lives_str.c_str();
-			DrawText(score_text, 20, SCREEN_HEIGHT - 20, 20, RAYWHITE);
-			DrawText(lives_text, SCREEN_WIDTH-20, SCREEN_HEIGHT - 20, 20, RAYWHITE);
-		EndDrawing();
-
-
-
-		if (highlight_index >= selected_word.size()) {
-			highlight_index = 0;
-			int rand_index = distr(gen);
-			selected_word = Dict[rand_index];
-			state_of_char.assign(selected_word.size(), StateChar::DEFAULT);
-			text_size = MeasureTextEx(GetFontDefault(), selected_word.c_str(), font_size, spacing);
-			start_x = SCREEN_WIDTH / 2 - text_size.x / 2;
-			start_y = SCREEN_HEIGHT / 2 - text_size.y / 2;
-			score++;
-		}
-
-		if (lives <= 0) break;
-
-
+	
 
 	}
 
